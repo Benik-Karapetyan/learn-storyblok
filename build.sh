72a7c12b281fa6015a7d357ac47c9bed9e9e5164
Benik-Karapetyan <benik.karapetyan1@gmail.com>
2024-03-18 15:35:08 +0400

    changed commands

--- build.sh
+++ build.sh
@@ -2,13 +2,13 @@ if [ "$CF_PAGES_BRANCH" == "main" ]; then
   # Run the "production" script in `package.json` on the "production" branch
   # "production" should be replaced with the name of your Production branch
 
-  yarn run generate
+  npm run generate
 
 elif [ "$CF_PAGES_BRANCH" == "dev" ]; then
   # Run the "staging" script in `package.json` on the "staging" branch
   # "staging" should be replaced with the name of your specific branch
 
-   yarn run generate-dev
+   npm run generate-dev
 else
   # Else run the dev script
   npm run generate-dev 
